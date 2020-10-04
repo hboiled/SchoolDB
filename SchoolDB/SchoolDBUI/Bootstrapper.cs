b90c9099ab4ae6efff67502f0fa6363c4a53f728
Sam Lee <sflee36@yahoo.com.au>
2020-10-04 23:04:58 +0800

    Added parts of ui skeleton and set up CM & MVVM

---
+++ SchoolDB/SchoolDBUI/Bootstrapper.cs
@@ -0,0 +1,67 @@
+﻿using Caliburn.Micro;
+using SchoolDBUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace SchoolDBUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer container = new SimpleContainer();
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // Inits base view model with corresponding view, 
+            // whose ctor must be parameterless (the one with initialize method call)
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        // DI container
+        // Sets an instance of itself to itself, passes out this reference to requesting components
+        protected override void Configure()
+        {                        
+
+            // Get from caliburn micro, a singleton of WindowManager and EventAggregator
+            container
+                // specify interface, provide implementation
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>()
+                //.Singleton<IAPIHelper, APIHelper>()
+                ;
+
+            // reflection on current application instance
+            GetType().Assembly.GetTypes()
+                // get types that are classes and end with view model
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                // 
+                .ForEach(viewModelType => container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
+        // Caliburn micro set up DI for an instance
+        protected override object GetInstance(Type service, string key)
+        {
+            return container.GetInstance(service, key);
+        }
+
+        // Caliburn micro set up DI for instances
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            container.BuildUp(instance);
+        }
+    }
+}
---
+++ SchoolDB/SchoolDBUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolDBUI.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        private IEventAggregator events;
+        private readonly SchoolDataViewModel schoolDataViewModel;
+
+        //private IAPIHelper _apiHelper;
+
+        public ShellViewModel(IEventAggregator events, SchoolDataViewModel schoolDataViewModel)
+        {
+            this.events = events;
+            this.schoolDataViewModel = schoolDataViewModel;
+            this.events.Subscribe(this);
+
+            // IoC inversion of control container can be accessed without the simple container for DI
+            ActivateItem(IoC.Get<SchoolDataViewModel>());            
+        }
+    }
+}
